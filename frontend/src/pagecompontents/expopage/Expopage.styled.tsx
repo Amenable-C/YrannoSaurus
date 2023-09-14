66ddb762d3387868b8ca9bfc5a7b0f662230aa0d
신종혁 <shinjh4903@gmail.com>
2023-09-14 17:56:39 +0900

    ✨ 엑스포 메인 페이지 작업 중
    Former-commit-id: bb70dfba9ae988c34b6e9f37ba639819590102fa

---
+++ frontend/src/pagecompontents/expopage/Expopage.styled.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a23b81ec3d6d047a852770b49f9f53267cb0fde479557e38ad2ee73971e9630
+size 706
---
+++ frontend/src/pagecompontents/expopage/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0237e10106d2cb9d53118c80ec5ea235a3c1d889109e61ae7ee62d9f11912ac3
+size 522
