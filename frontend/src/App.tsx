c81ff02c5e70c680b667754c59eec8217026d7f7
김예슬 <pdy051257@naver.com>
2023-09-15 17:56:06 +0900

    :sparkles: 고성지도셋팅
    카카오맵띄우기
    
    
    Former-commit-id: 25ff022a47143ae1f67c61ee0f562e0f11cff024

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Expo from './pagecomponents/expopage';
-import Dinosaur from './pagecomponents/dinosaurpage';
-
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/Expo" element={<Expo />} />
-        <Route path="/Dinosaur" element={<Dinosaur />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4b626e9afd4ac99933d2ff1800b21efa3b4e73fd0ac6dd4e4daac458aba1d6d
+size 538
