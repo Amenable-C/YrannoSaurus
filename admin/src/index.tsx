a426b4e0c5e30d61cda339f68f9bdc572b9ff95c
0214kbs <0214kbs@naver.com>
2023-09-15 10:56:23 +0900

    🎨 폰트 추가 및 default theme 설정
    Former-commit-id: 3964876ad21373aa8d01e00233ae7c9ece1a2d43

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1df70e7d41d5819c44746d3d95149f9709442fc7865ec218d6d18f6cc48f961a
-size 551
+oid sha256:d714aaa40dd3887152512cd82518d19c0e6da69ca20ff5f892065df7e50f21f0
+size 777
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c88af813bb89ef847d37a23cc7816df28bec376d6bc6eaba5411db14081b7520
-size 799
+oid sha256:d714aaa40dd3887152512cd82518d19c0e6da69ca20ff5f892065df7e50f21f0
+size 777
