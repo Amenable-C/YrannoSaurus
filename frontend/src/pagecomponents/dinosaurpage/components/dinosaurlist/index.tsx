074def8cc9e1129312b74a2b197b34e44ba970e1
alsuddlek1 <minyoung99311@gmail.com>
2023-09-15 11:04:18 +0900

    :sparkles: 공룡카드 세
    공룡리스트 속 공룡카드 셋팅
    
    
    Former-commit-id: f75429d8f8c05c4a8e33294145f61f22b5bde3ae

--- frontend/src/pagecomponents/dinosaurpage/components/dinosaurlist/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/dinosaurlist/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2322e0bc46358a24994f9fb64bbee32ffbecd7d2c79f54aee67121dcab474a6f
-size 556
+oid sha256:abf1795c9008147309bd64a4ed3dccd30aea97a1fde445b1c2e1f075ae3ba71b
+size 608
