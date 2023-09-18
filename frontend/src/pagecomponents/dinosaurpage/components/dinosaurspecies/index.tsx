f0c8d0dd90d994909b4e400359a3264bfccb502b
alsuddlek1 <minyoung99311@gmail.com>
2023-09-18 12:31:52 +0900

    :sparkles: 공룡 detail 셋팅
    공룡 Card->Detail Modal세팅
    
    
    Former-commit-id: 1d6e76a759b90f9abd6ce4e8b68903e116b65037

--- frontend/src/pagecomponents/dinosaurpage/components/dinosaurspecies/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/dinosaurspecies/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e23227d5fab809124b506d67ee1d9a8de32839103741dd899a05f3eaf9e98c
-size 3218
+oid sha256:5a5884cc8a98c18c151b9cc006f85da1c2f63f7c51fce9026dc1e861f718721f
+size 3231
