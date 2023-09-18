4be1efd17400339fcda451e143b783f885f2cc4f
minyoung <minyoung99311@gmail.com>
2023-09-18 21:36:54 +0900

    :lipstick: 공룡 디테일 css 변경
    공룡 디테일 내용 임시내용, 스크롤바 삭제필요
    
    
    Former-commit-id: 3bbb21c18dc15fe6b9358e8616e54c5093183d70

--- frontend/src/pagecomponents/dinosaurpage/components/dinosaurdetail/DsDetail.styled.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/dinosaurdetail/DsDetail.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:042eac63229a5eb86ae7e0b9f918fb938af529cf48ca2a79e5816949950493e4
-size 1494
+oid sha256:e8ae0d1d31776bd169ec0264b776d3bb74682ed58dfc34acb7e9a2e1def06df5
+size 1651
--- frontend/src/pagecomponents/dinosaurpage/components/dinosaurdetail/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/dinosaurdetail/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2be00a34b9baf944396d977e1fc6e294648daee924774ffbd9130678619886e
-size 1670
+oid sha256:4d5e1655e2a67269f1fbd6768878aa06f6d3334ff9a9597011b6e3a70dd0a18a
+size 2078
