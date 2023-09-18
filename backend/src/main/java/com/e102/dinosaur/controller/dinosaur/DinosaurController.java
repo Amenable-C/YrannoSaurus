b4373b3a3f88ca4fb5d6c895d0b4f350d3546604
최연재 <duswo0624@naver.com>
2023-09-18 16:00:24 +0900

    :sparkles: 공룡 상세 정보 조회 기능 구현
    Former-commit-id: da4fe0df1ca79c7af0b7320cea11ba98c060ad33

--- backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
+++ backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e95d0d5682bf4f67201b49b66f96de5e7830b14098da56392f78c19a57a5ebdb
-size 828
+oid sha256:4acf0f8dcf0fa7931d2197a9bda65263cdaa12579cb03c7e28de3807e0a18ea1
+size 1155
