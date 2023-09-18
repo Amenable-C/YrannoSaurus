09d064ae1b7e4b278f431c7ea2ce995d885440fe
최연재 <duswo0624@naver.com>
2023-09-18 16:21:27 +0900

    :fire: 전체 관광지 조회 시 내용과 주소는 보내지 않는다
    Former-commit-id: 40de28b6f8ed7b54799d24b56f3a9f8fc9f87bc3

--- backend/src/main/java/com/e102/dinosaur/service/tour/response/TourResponse.java
+++ backend/src/main/java/com/e102/dinosaur/service/tour/response/TourResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bae920f247e1234f6fc6761513bf08de6578c53de5b109a240eaf91412852e52
-size 1077
+oid sha256:eaac687ff5100c5a7a46dc9e57cf268dcd80ff4631a03417844b4cb4f6506d35
+size 825
