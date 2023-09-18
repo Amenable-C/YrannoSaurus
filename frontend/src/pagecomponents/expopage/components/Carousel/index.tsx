e2e05f7b6a28f343139d59c845791b99fd7f4134
신종혁 <shinjh4903@gmail.com>
2023-09-18 17:50:10 +0900

    ✨ 엑스포 개장시간 레이아웃 구현
    Former-commit-id: 7a5f32018590c4588dab01c20386d5fb639b17f3

--- frontend/src/pagecomponents/expopage/components/Carousel/index.tsx
+++ frontend/src/pagecomponents/expopage/components/Carousel/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7663adfe472f5211bfe1ec0405b2f9f1cedd2ffdf7484699ca475ccdd80ca064
-size 2638
+oid sha256:ef2730121c89b0215a39ecd6850a63d37150aad7ab102c4512f848a11e736ca2
+size 2675
---
+++ frontend/src/pagecomponents/expopage/components/eventvenue/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:885d0902febb12e02d134bfdf866be2a33af105ec32b49f16b297e6d053c7192
+size 728
