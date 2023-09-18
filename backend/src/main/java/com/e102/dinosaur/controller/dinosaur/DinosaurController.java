2ccb3b5411aee888234f3918b6b764aceefcdab6
최연재 <duswo0624@naver.com>
2023-09-18 16:01:18 +0900

    :sparkles: 이름을 활용한 공룡 조회 기능 구현
    Former-commit-id: 4b3f67a2211009ab179b1f707ac438eb5f717266

--- backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
+++ backend/src/main/java/com/e102/dinosaur/controller/dinosaur/DinosaurController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4acf0f8dcf0fa7931d2197a9bda65263cdaa12579cb03c7e28de3807e0a18ea1
-size 1155
+oid sha256:6f709da7ced342179a7c6f68b1b5eda5ea94b6c31ed9959472b682180b67296a
+size 1478
--- backend/src/main/java/com/e102/dinosaur/service/dinosaur/DinosaurService.java
+++ backend/src/main/java/com/e102/dinosaur/service/dinosaur/DinosaurService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:813916d2d517aaf8abc2414b9d89be5c398c059f0d5a33e66dde678bbcc62855
-size 1336
+oid sha256:9dfd1c0b3fc5e002f4bd96963b2d13c44ec353391527b75e98b9f08c8bc25246
+size 1794
