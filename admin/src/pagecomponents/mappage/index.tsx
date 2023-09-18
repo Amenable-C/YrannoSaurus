6465a9abe99700244f7c669f4a755d5e50e90617
0214kbs <0214kbs@naver.com>
2023-09-18 14:02:53 +0900

    ✨ 왼쪽 카테고리 목록 사이드바 추가
    Former-commit-id: 5dbc004d947b7c70cb691dcbfa07f3cb636bfa8b

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,13 +1,3 @@
-import { Map } from 'react-kakao-maps-sdk';
-import Sidebar from './components/sidebar';
-
-const MapPage = () => {
-  return (
-    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-      <Map center={{ lat: 35.056820163386156, lng: 128.39756122959787 }} style={{ width: '100%', height: '100%' }} />
-      <Sidebar>right sidebar</Sidebar>
-    </div>
-  );
-};
-
-export default MapPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:84416709c223a3f551f49111392427c09fd507c491e3ec063b0ca3e299e90dd7
+size 549
