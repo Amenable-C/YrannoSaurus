b92d543c946203423a6341910f50b28095d391b0
김예슬 <pdy051257@naver.com>
2023-09-18 17:49:34 +0900

    :sparkles: 레이아웃 초기 세팅
    전체적인 레이아웃 잡기
    
    
    Former-commit-id: f25a8d4346b64fbff4927333aaf8671c95fc7a62

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
@@ -1,20 +1,3 @@
-import React from 'react';
-import { Map } from 'react-kakao-maps-sdk';
-import { StyledKakaoMapContainer } from './KakaoMap.styled';
-
-const GosungKakaoMapComponent = () => {
-  return (
-    <StyledKakaoMapContainer>
-      <Map
-        id="map"
-        center={{
-          lat: 35.05935949688545,
-          lng: 128.39945815291523,
-        }}
-        level={3}
-      />
-    </StyledKakaoMapContainer>
-  );
-};
-
-export default GosungKakaoMapComponent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4131dcacc1b949550e26f4503edfc3e978d21d1c58c6072fc7b63c9fb17b4fc4
+size 527
---
+++ frontend/src/pagecomponents/gosungpage/components/sidebar/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0ab62b6d3db65d97090dd4d5b3eb3a51c1faef527d0b0c248cc48f2b1187072
+size 583
