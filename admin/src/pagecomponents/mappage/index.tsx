c35155914d1b4d4c3fb2eabd245019500863220c
0214kbs <0214kbs@naver.com>
2023-09-18 16:20:44 +0900

    ♻️ type 분리
    Former-commit-id: 25ec53e7daefda7fc7d90c9b2560fc0ff49eb7b4

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd8d7379c55612a3c19ddf20283805c420e86e7dfb413a44adb77bc7fb393e0f
-size 1185
+oid sha256:7e05758a21f871b9abe1ec213ca91737e40225cda4cd459733ea33757a040725
+size 1143
