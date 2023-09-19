e673363017bad99e88dd4665abc3b6eeaafa57c2
0214kbs <0214kbs@naver.com>
2023-09-19 09:36:37 +0900

    ✨ 입력폼 저장 기능 추가(콘솔창 출력 대체)
    Former-commit-id: 3d3e56150b06f4a8ff2cb4167dbca68bb8184f07

--- admin/src/pagecomponents/mappage/components/sidebar/index.tsx
+++ admin/src/pagecomponents/mappage/components/sidebar/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0b103ba0f52ce755b6988a4acbe59867e1aa84045ac492d753474bb736f24fa
-size 1435
+oid sha256:38a0f9893f4c51ec09aee725863840a2e2b58fdcc165cebb6532d815b6e79366
+size 2144
