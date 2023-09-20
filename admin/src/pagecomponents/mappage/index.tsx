9c260f965abb4720afbb7ec0f688980c81ed8603
0214kbs <0214kbs@naver.com>
2023-09-20 14:52:54 +0900

    💄 marker 위치 조정
    Former-commit-id: 59dc3947b3126900fd1d4a1ee70d75b5bcbe2f37

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb9486a3fa7caca1deaffc3b2f3f593cab2360a4a932da7052a575d14fac8a9
+oid sha256:f3c9de99bd61b9d85328b187df381a73d74de3bd855bd9e684b3cd69eee72463
 size 1373
