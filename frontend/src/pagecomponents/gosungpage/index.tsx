a90a9419e4b258c98b7e2eff088cb22849e942e7
김예슬 <pdy051257@naver.com>
2023-09-20 17:57:18 +0900

    :sparkles: 사이드리스트 초기셋팅
    사이드리스트 초기셋팅
    
    
    Former-commit-id: 34d2bf360a61383128256a11691934a74e0f1e45

--- frontend/src/pagecomponents/gosungpage/index.tsx
+++ frontend/src/pagecomponents/gosungpage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbaa6c48c92339327b0e0a2fceb067187acc2f814152aa231e5e923c7db269fc
-size 750
+oid sha256:3542f104fa2768754aa2631358d178574104d669ebccee89a37e1675bcac31a7
+size 802
