27ee6b3f4b31d9061e6c3023dd01a2c8cf6f5411
yeseul0722 <pdy051257@naver.com>
2023-09-22 03:00:58 +0900

    :sparkles: 고성 사이드 리스트 초기셋팅
    고성 사이드 리스트 초기셋팅
    
    
    Former-commit-id: 60360923d731e8d79397d1fc318efdef14b4ac4c

--- frontend/src/pagecomponents/gosungpage/components/foodcategory/FoodCategory.styled.tsx
+++ frontend/src/pagecomponents/gosungpage/components/foodcategory/FoodCategory.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74e502ba5e6d181b3ca208dd1a149224d41d3d352b3214900f277b2aab62bf98
-size 604
+oid sha256:6eba48d8c732b71599595254d477f1a659e060e4ee01237cca4dcfb427a87e89
+size 820
