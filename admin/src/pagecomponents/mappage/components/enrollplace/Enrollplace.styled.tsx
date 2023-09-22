3332daccf9f18c38382a3c8cef97e1051739d657
0214kbs <0214kbs@naver.com>
2023-09-22 12:17:55 +0900

    💄 사이드바 디자인 - 입력하는 부분만 스크롤 생기도록 구조 조정 - 저장하기,삭제하기 버튼은 사이드바 하단에 고정
    Former-commit-id: cfe1edc099cbc9ca98d0c0cd794a6e9461b295e0

--- admin/src/pagecomponents/mappage/components/enrollplace/Enrollplace.styled.tsx
+++ admin/src/pagecomponents/mappage/components/enrollplace/Enrollplace.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4942afa390bc692cdeec5842dc98b72065b3ffdd46758cbcd03bb1da4c80b68
+oid sha256:e4a8fc5926b942e654a40d63d9cbe7d675e468f87cdc7218bf8c4c204c5a1d69
 size 1413
--- admin/src/pagecomponents/mappage/components/enrollplace/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollplace/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9627627e5bb50a87af3f668448afb70586e7b78fd0fe582f5e2eb00175eeb0ea
-size 4470
+oid sha256:3b260214aeedb0ed1458318b689f39c0d6886527916ca9b2c2d2159d9fc799ae
+size 4615
