50ed29bfdee0fe1da1af301b9f4664f27ce92134
0214kbs <0214kbs@naver.com>
2023-09-22 12:36:24 +0900

    ✨ 로그인 실패 메시지 추가하기
    Former-commit-id: 6b442dc74f61b9b78d118a36fddcaf65369db62e

--- admin/src/pagecomponents/mainpage/index.tsx
+++ admin/src/pagecomponents/mainpage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71eac16e6b950f0bf959fc20e0599f689e01bb594e067665a5e61cf317a9b7e8
-size 1436
+oid sha256:a9ebec13a0d9f8742509f3361e63a6bfb722c4d60ce94837cd6d8b9627868fa3
+size 1620
