a8285388294594a41aab818703a3d38b45ab6e3f
신종혁 <shinjh4903@gmail.com>
2023-09-22 09:59:54 +0900

    ✨ 고성 관광지 모바일 기능 구현 진행
    Former-commit-id: 207edc4bd54b10cfde1dfe105787e2bdec57ae04

--- frontend/src/mobilepagecomponents/gosungmobile/components/list/index.tsx
+++ frontend/src/mobilepagecomponents/gosungmobile/components/list/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe63fd75445b7f04834de248a8677b7670fed793b6727cebbd1d083c2f2dd6a
-size 948
+oid sha256:cd84c0d7ff5b52a46fcc01a178a07ebe93b60218e8cc6bb16b665dd8029aea29
+size 1259
