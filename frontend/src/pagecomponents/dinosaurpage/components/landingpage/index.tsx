277b0a90b1aaf5f6c29dc238dc07471bf00010d1
alsuddlek1 <minyoung99311@gmail.com>
2023-09-24 00:01:48 +0900

    :sparkles: Landing 셋팅
    기본 구성, ui
    
    
    Former-commit-id: cfc10586d825da81238cae3d8d9f638dd6b2addf

--- frontend/src/pagecomponents/dinosaurpage/components/landingpage/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/landingpage/index.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { StyledDsLandingPage } from './LandingPage.styled';
-import Globe from 'react-globe.gl';
-
-const LandingPage = () => {
-  return <StyledDsLandingPage>랜딩페이지</StyledDsLandingPage>;
-};
-
-export default LandingPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:a420f0a94b7a999bfb45986a22c80a0ff4d0a355ee7cc19f38366fe59d25c6b9
+size 1198
