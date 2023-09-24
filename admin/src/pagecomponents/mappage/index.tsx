c52b38897b09104b0876e8bd85f8e75dc8ef294b
0214kbs <0214kbs@naver.com>
2023-09-24 23:40:31 +0900

    💄 마커 위치 조정
    Former-commit-id: 4e1fef2f9c0a2f57562e702288504f801db53855

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8c11405997565ee10460dfb9749f75bc72146bf52f0a1e1433e3a3c46e84449
+oid sha256:53ca5cbbe8c570a10ce835b874c93ae02ce07aa18feb1e0f020f49fa1fa64c33
 size 2536
