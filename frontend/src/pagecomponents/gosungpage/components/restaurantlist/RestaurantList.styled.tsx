8b4284d80d0a0583ff4841ca0810b2ae3b16334c
김예슬 <pdy051257@naver.com>
2023-09-25 09:57:39 +0900

    :lipstick: 레스토랑 리스트 css 수정
    레스토랑 리스트 css 수정
    
    
    Former-commit-id: c271747f9fff61f9cd64c27c481b2ab43a4a79ca

--- frontend/src/pagecomponents/gosungpage/components/restaurantlist/RestaurantList.styled.tsx
+++ frontend/src/pagecomponents/gosungpage/components/restaurantlist/RestaurantList.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:500ad18f814390e43c6f8bf0ddd6762e2a73bf7d9c959dcce1751437fb3b196a
-size 732
+oid sha256:1f5c3b103cc03126b8dc850c1ecebd464494a99de1e5427778e0ba89fa7c67b5
+size 911
--- frontend/src/pagecomponents/gosungpage/components/restaurantlist/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/restaurantlist/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69d369d8cceae9f05284adda68d19ad43886376e85ee96fbc2009eeb229fc939
-size 1392
+oid sha256:5ec6a1253090e0b96155ff1094e6e76720474a74b7b193786a6a97be7a744166
+size 1487
