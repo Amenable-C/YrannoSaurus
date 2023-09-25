4f61c9e5077864f53e36e6bc9e869e4aa019d51f
slbin <smpts00@naver.com>
2023-09-25 10:46:35 +0900

    :sparkles: 공연 수정 api 추가
    Former-commit-id: 37c8ab1ddfba4c44da666476c053a1894f51dc05

--- backend/src/main/java/com/e102/dinosaur/domain/festival/Festival.java
+++ backend/src/main/java/com/e102/dinosaur/domain/festival/Festival.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baa0e1e77914717975240f5166bb34368dc5e6afa19c2a32ab723cbee6ffbb3c
-size 857
+oid sha256:b4b140548423753ad0f2fc6f3ac8add9b20c796a2645809324b5d6e244c48d2d
+size 1114
--- backend/src/main/java/com/e102/dinosaur/service/place/FestivalService.java
+++ backend/src/main/java/com/e102/dinosaur/service/place/FestivalService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3081a1c4baac6bd0c6b8cb60d2c9225f301072f4600e48694c7c9215862c632a
-size 2073
+oid sha256:0e844717ed997110fd03dc87e48f3d95542545ef3fb237f3926584fd3983ccff
+size 2624
