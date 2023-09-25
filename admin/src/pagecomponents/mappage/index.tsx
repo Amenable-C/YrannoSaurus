efb747a9eb94e55565bc10f778bed55730e085b0
0214kbs <0214kbs@naver.com>
2023-09-25 11:07:33 +0900

    🐛세부사항이 초기화 안 되는 문제 해결
    Former-commit-id: 46e3a299fe3a0976de29084a307e4e1d56f3682b

--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82cbff867014216161050ad33c69e46dc1358c40fa0521e3045d4126dddb8cf3
-size 3145
+oid sha256:14af63aeccd124341debe14da6fda04c9da99a260bd12284b4b98d30792a37a0
+size 3162
