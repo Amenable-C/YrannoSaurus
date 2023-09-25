45e4d69b77e05f6b34f1acf0896665f45842dca8
신종혁 <shinjh4903@gmail.com>
2023-09-25 10:00:16 +0900

    ✨ 공연정보 API요청 기능 구현
    Former-commit-id: 2527382f57a829e13d03b98a4c867a194c9420df

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7e23007b8b093909050ad4cf9591e461e515fd6cc9c31b5803c3a47ba71facc
+oid sha256:8320fcd26df494c491db513107ef8213aeef824afc293a3b51625ee9bafea413
 size 854
--- frontend/src/pagecomponents/expopage/components/performance/index.tsx
+++ frontend/src/pagecomponents/expopage/components/performance/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b6afe23668f505c25ad9d225bbfbb21daa03b7448cf17b486f5556c39092e74
-size 1438
+oid sha256:7015285d6dd7bc63dd77f68514959149367922fe0192ba39a1dfdeac370c1f30
+size 2138
