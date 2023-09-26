7cb3b73fa2921ec1474448000558678f19a1dbac
0214kbs <0214kbs@naver.com>
2023-09-26 12:15:22 +0900

    ♻️ 컴포넌트 재분리
    Former-commit-id: 69936e3fc63ddd10ede80457d73a0ad73eab78bd

---
+++ admin/src/pagecomponents/mappage/components/enrollmap/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ae4e9e7f1596244126ecc74a279c62720aa273717b953cf7ef930ee602c8df0
+size 3364
--- admin/src/pagecomponents/mappage/index.tsx
+++ admin/src/pagecomponents/mappage/index.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7c5c5dcbbd6774810e55b014801bb0b7fb89fe9d25163a9bd2f1ffc542f53cc3
-size 3370
+import { useCategoryStore } from '../../stores/useCategoryStore';
+import EnrollMap from './components/enrollmap';
+
+const MapPage = () => {
+  const { selectcat } = useCategoryStore();
+
+  return <div>{selectcat === '1' && <EnrollMap />}</div>;
+};
+
+export default MapPage;
