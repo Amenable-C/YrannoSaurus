88fe96f6c8c0b8b0562cac4380b551eab2690868
alsuddlek1 <minyoung99311@gmail.com>
2023-09-27 01:16:46 +0900

    :lipstick: 코리아노사우루스 ui 업데이트
    코리아노사우루스 ui 업데이트
    
    
    Former-commit-id: 2cf412904b04349918b323213fd62dced452b38c

--- frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/Koreanosaurus.styled.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/Koreanosaurus.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5951c30c5d7ec24a9b9d341d87748b9febb46509edb759a025939555e6016a72
-size 4710
+oid sha256:7964708a6e1e1ec42c14e06db90390bc75a1f154b9778d4222d6551199aeddde
+size 5141
--- frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a7273277484231e107ed80e1b7f8073fecf342f897eaef96bd1e7bd989f644e
-size 4671
+oid sha256:c061aba5da126c1e2b06d630d8bef4a6ac4db36a29070cc47a7efca4711dda33
+size 4541
