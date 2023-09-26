07577fe7e35924a27fc53c7ee823dd58f3921d21
0214kbs <0214kbs@naver.com>
2023-09-26 17:41:12 +0900

    ✨ 공연정보 표시
    Former-commit-id: 40b0507b2698154f7772d99ece54cbe30c2af29e

--- admin/src/pagecomponents/mappage/components/enrollshow/EnrollShow.styled.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/EnrollShow.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e87c7c40da3e196dfa574a913e393289b815ee1c2d2cb9eb8d846c3005b337ab
-size 842
+oid sha256:02985efdc98fba2980e8218edb5db5cac089af8b4dd8546dee721df9549807ad
+size 1132
--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e17d9d5d78a6146752cc7c27caa07c2413cea4bdd23e33f3aacda4436e09b7b
-size 916
+oid sha256:46800363c2fdae810b705e063118d07bbe6ba842503d3237b4e0e0f444c888a1
+size 2442
