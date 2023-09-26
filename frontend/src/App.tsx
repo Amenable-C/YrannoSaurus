2f184f125051e4976bbf991d6488f2e10c79399c
alsuddlek1 <minyoung99311@gmail.com>
2023-09-27 02:24:40 +0900

    :sparkles: 전체 랜딩페이지 셋팅
    전체 랜딩페이지 셋팅
    
    
    Former-commit-id: 7772e3943433377c4d9e10f5c2813b6d0f6d4787

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7e23007b8b093909050ad4cf9591e461e515fd6cc9c31b5803c3a47ba71facc
-size 854
+oid sha256:8bec32c4294355a0b4b449e21a9080cc6cf7f447a5576fde0d9985c55636217d
+size 963
---
+++ frontend/src/pagecomponents/mainlanding/components/splitpage/indeex.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39876cece06034b20afa8043ebd1196bd7b1925af34c0b16805d897a4182fd2e
+size 1534
