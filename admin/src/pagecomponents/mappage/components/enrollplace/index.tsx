93a8e1db9f51a9204a128856553a2c4821512e68
0214kbs <0214kbs@naver.com>
2023-09-26 11:25:54 +0900

    ✨ 장소수정 기능 추가 - 서버 연동 - 수정하기 클릭시 장소 수정
    Former-commit-id: 8f59cbd54bf11b92dcf36ca14a0a49f336ec4af9

--- admin/src/pagecomponents/mappage/components/enrollplace/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollplace/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a185d6e8722c75802d2a58e3560e35ce9a98871f08c32f15eddec3f9ed1199f3
-size 8216
+oid sha256:c29810ccffae4404d8687a303ccd6a36e1912f3e2909e34cf4810a237a4d7dc0
+size 8938
