1fc9f157fefcbd64a3ddcf20e59416b5701140d8
김예슬 <pdy051257@naver.com>
2023-09-27 09:18:12 +0900

    :lipstick: 카테고리 css
    카테고리 css
    
    
    Former-commit-id: 9769208f045aca9ee15adeaa9f79f3f2dbe414e0

--- frontend/src/pagecomponents/gosungpage/components/accomocategory/AccomoCategory.styled.tsx
+++ frontend/src/pagecomponents/gosungpage/components/accomocategory/AccomoCategory.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca3a33b6bc436b7e23f88b41d609154558752b83963b3edb53c8cfeca5b6c06a
-size 651
+oid sha256:1a3444d921048945c8ec0b6dab5ad9293c8affae4dd12f029922b7254da0ea3c
+size 800
