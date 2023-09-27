93587971b1699b30f3763b58f5f168072dd17014
0214kbs <0214kbs@naver.com>
2023-09-27 14:49:46 +0900

    ✨ 공연정보 저장 api 연동
    Former-commit-id: 0ea6c2fb7b612070b34d1414cf0ede7149ed6c57

--- admin/src/pagecomponents/mappage/components/enrollshow/EnrollShow.styled.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/EnrollShow.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb9eec5d4034722d7dbb5de56f3bf69c85f54d064e11725e41270bf4a6db0d5
-size 1447
+oid sha256:747e1bee5a91b82b57300c4df8b12a818e9d34094eaa586ff02c5e31fb22d717
+size 1044
--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:371ec6a423ae9b7ed54e70a97052d7edc3e2ccfc2a2df71601ecf4f93a28e09c
-size 4193
+oid sha256:2f8feea5ce81a558669f11fa86827eeb3a83e242e5c9ae3bbad7ce0c2ebb9e76
+size 4343
