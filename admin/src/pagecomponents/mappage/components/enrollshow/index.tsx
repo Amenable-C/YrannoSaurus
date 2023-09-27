9b5846209523f3fc3e085638ca8fb61b94de0594
0214kbs <0214kbs@naver.com>
2023-09-27 09:28:10 +0900

    ✨ 공연추가 입력필드 생성
    Former-commit-id: 67cb39afee88c0d65e9542432e124a9582ad419d

--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c01b50b87a728fd414e02beeabc8951e4a398438fa47e82f8e8d2e9319a8135c
-size 2160
+oid sha256:c6624aa3c633eb69b3f1db4b8fb240bc645126200907d28bf842f95b9dba130a
+size 4274
