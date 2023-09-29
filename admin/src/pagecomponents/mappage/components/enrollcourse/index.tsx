f9d3aa1daf5b1a846903b17af2fc5ec97d79f19e
0214kbs <0214kbs@naver.com>
2023-09-29 21:47:22 +0900

    ✨ 클릭한 코스의 코스 정보
    Former-commit-id: b95b408f93de0bfaf8a2f4bd466c99d5334e1b90

--- admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f93c879aafc3fdeb6320b2dcfcaa40cd861d9756f5f3281a0afd593dbc8b30ee
-size 3615
+oid sha256:45baa13bbe40231962f90efe079ab8dd29e5e044ef74eb38167ae2b917e5593a
+size 4608
