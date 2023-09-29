3c7d8ff6a138cca10f28da069f6b39b618d3e633
0214kbs <0214kbs@naver.com>
2023-09-29 21:53:18 +0900

    ✨ 저장 버튼 클릭시 서버에 전송할 데이터 형태로 변환
    Former-commit-id: 2c9b449c9dede142d5089de7d88224b2e73eb05b

--- admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45baa13bbe40231962f90efe079ab8dd29e5e044ef74eb38167ae2b917e5593a
-size 4608
+oid sha256:7fd8d0e4fc74918ff243589e883853c07ee2d34343d2970afe11b7cc061ae9f4
+size 5645
