6914fd443269a220abe4d8b3518e20b6aa4c9026
0214kbs <0214kbs@naver.com>
2023-09-29 23:43:41 +0900

    💄 width 통일
    Former-commit-id: 33326c6d7e79c16bd908af6471c7f8ad27eaa4f3

--- admin/src/pagecomponents/mappage/components/coursemap/index.tsx
+++ admin/src/pagecomponents/mappage/components/coursemap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ebb38fb907cb015893488e55bd1f7f39693524ab30879f98187577fc64605eb
-size 3022
+oid sha256:90bf9a8355eb9e2dccb077569ccf56fb63f94005e7484c10283e1a98f745383c
+size 2933
--- admin/src/pagecomponents/mappage/components/enrollplace/Enrollplace.styled.tsx
+++ admin/src/pagecomponents/mappage/components/enrollplace/Enrollplace.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aab9f03018960a7d6bfbf14da5e7a02027bf32e6ec8108ab562ea6ed9615ab4
+oid sha256:f4310a3b5f7fc1ab17a0b2f84639f681de31e2f17c9cf4e5794b41bcf529b6ac
 size 1943
