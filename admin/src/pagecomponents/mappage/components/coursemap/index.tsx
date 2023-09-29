05450c05f3af187d869c6ef7e17dfb47f394402b
0214kbs <0214kbs@naver.com>
2023-09-29 23:35:12 +0900

    ✨ 코스 저장/삭제 시, 코스 정보 재요청
    Former-commit-id: b72f314e9957dc81a9b89cd61c98cfabf2a18fca

--- admin/src/pagecomponents/mappage/components/coursemap/index.tsx
+++ admin/src/pagecomponents/mappage/components/coursemap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3cba404a5e3b7018963f5172a22b76f33cd9786aa4d91ec7045b34ab93900f1
-size 2932
+oid sha256:3ebb38fb907cb015893488e55bd1f7f39693524ab30879f98187577fc64605eb
+size 3022
--- admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollcourse/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ec1ac36773b3ab9a0d52b338f473c1ab6c292181038ef4de96436f7f59dd266
-size 5184
+oid sha256:6fa5356dcfb3aa6d84d5d039c2cb96cc178060bcb74feefadc4769dbebd60461
+size 5374
