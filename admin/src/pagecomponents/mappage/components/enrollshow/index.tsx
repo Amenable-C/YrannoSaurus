55f17ba0816b348950b5669e176de8714946a2aa
0214kbs <0214kbs@naver.com>
2023-09-30 01:29:51 +0900

    ✨ 입력 누락에 대한 유효성 검사 및 경고 추가
    Former-commit-id: c1789f55801184084d74629db395b11a62d27d26

--- admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
+++ admin/src/pagecomponents/mappage/components/enrollshow/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6699ffb9bd67ad1a926fe9ae3a3153ff2cc66dfbf9c128750790010912442782
-size 4851
+oid sha256:e866d0210f1d5f68800e8b76c598dd6590e0618c125d60a453f4c1bda57bcafc
+size 5015
