46f849d4dda95535f364b93bec031845861005d7
김예슬 <pdy051257@naver.com>
2023-09-30 10:49:32 +0900

    :bug: Import 경로 오류 수정
    import문 경로 오류 수정
    
    
    Former-commit-id: fbf24d5405c9d193c673c2a23acd0fbbfb9b010c

--- frontend/src/pagecomponents/gosungpage/components/accomocategory/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/accomocategory/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3f488176baed90285c4255318bcdb6b69e4baa1bd98ffbcf46c2e8763f3f828
+oid sha256:e5da4c4092f62e71a625d4e80896e49922c51171cc5e2a638fc71979e45beac3
 size 2859
