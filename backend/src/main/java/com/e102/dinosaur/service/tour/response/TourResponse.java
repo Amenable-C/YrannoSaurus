65562098bf37105e781891f3257afec7b5e031e6
Amenable-C <duswo0624@naver.com>
2023-09-30 17:25:50 +0900

    :sparkles: 카테고리별로 관광지를 조회할 때 주소 값 추가
    Former-commit-id: bf0734c74605dcfddd48cbe5b77035375b2a065d

--- backend/src/main/java/com/e102/dinosaur/service/tour/response/TourResponse.java
+++ backend/src/main/java/com/e102/dinosaur/service/tour/response/TourResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaac687ff5100c5a7a46dc9e57cf268dcd80ff4631a03417844b4cb4f6506d35
-size 825
+oid sha256:16d1b50aaa10fb5a66adc243337ba77b6123aa71ddfce3cd4063cd797ac575c5
+size 945
