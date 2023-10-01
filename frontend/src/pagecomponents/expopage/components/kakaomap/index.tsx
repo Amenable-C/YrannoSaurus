6f7ca5cddf0b27a2a1c8d9ae4f385b1d0fc23759
신종혁 <shinjh4903@gmail.com>
2023-10-01 21:50:55 +0900

    ✨ 엑스포 메인 반응형 구현
    Former-commit-id: 73b98791a3e67dc054ceb6e2215ff6d1a777ebd5

--- frontend/src/pagecomponents/expopage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/expopage/components/kakaomap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db06aba2488428f70297dc45e71d295913900f7a73bb9008b07cd177a4ee2e63
-size 2707
+oid sha256:b314e4b158ed047f8b1f58afc3b42a9563dcf356b151fb00a37b2929346945dc
+size 2706
