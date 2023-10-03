324e27f04a8f183533faa78ec60de95439dcefe9
alsuddlek1 <minyoung99311@gmail.com>
2023-10-03 21:59:33 +0900

    :sparkles: 동물 정보 셋팅
    동물정보셋팅
    
    
    Former-commit-id: 4fab034d38907cbc9cd273c27749d8224a35f78f

--- frontend/src/pagecomponents/dinosaurpage/components/animaldetail/animalDetail.styled.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/animaldetail/animalDetail.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7f24230ffd942f72f82582827209b8cea2b07d33a5b3fb9c328a41ace03cb5b
-size 1421
+oid sha256:04431a89bbbce2a1d369fcebf7f32fc2598ab0715b8d9b60291e4c6b8e1c19d8
+size 1470
--- frontend/src/pagecomponents/dinosaurpage/components/animaldetail/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/animaldetail/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59733844fce0ba5dad00a4307fa32375629b25333fd011349e3b9479b9f35004
-size 2949
+oid sha256:e1fb289183a6436fc12fba1f5b44cd33b2d8f9e9805b21a8dc39b8663003a02c
+size 3305
