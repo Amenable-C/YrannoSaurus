4305956e3885743dd4ef862a8ed52595c830a128
alsuddlek1 <minyoung99311@gmail.com>
2023-10-03 16:04:37 +0900

    :lipstick: 한국의 공룡 배경 이미지 변경
    한국의 공룡 배경 이미지 변경
    
    
    Former-commit-id: 21330e3e7c4208e32d8997773580943b4b0c0218

--- frontend/src/pagecomponents/dinosaurpage/components/koreamappage/koreaMapPage.styled.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/koreamappage/koreaMapPage.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18763067932bbd0d146b5e5956a4135e9c57c5340ddae26650d3e050f63bc619
-size 4655
+oid sha256:dbb7bbe5cf5bfc4f05846f0867b89acac96ad74e984eee8db9b3ca631f23410c
+size 4658
--- frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/Koreanosaurus.styled.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/koreanosaurus/Koreanosaurus.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2d81d8b5ae128b5dad7a42071ce5ce7fc9ad8fecf425133db326d8a9b488327
-size 5290
+oid sha256:b6a46f59db6b7408c40101b9de0b7eabaa6bd18ddcf8b7c50d535f4e672f9efd
+size 5277
