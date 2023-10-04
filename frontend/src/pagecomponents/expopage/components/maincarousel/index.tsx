e1848602b937018f577ccd6eb3df4e3c47d3c4e6
신종혁 <shinjh4903@gmail.com>
2023-10-04 16:15:04 +0900

    💄 엑스포 메인 페이지 디자인 수정
    Former-commit-id: 0d8d3754b4a6323f24c85c48fbb7e6821596b69b

--- frontend/src/pagecomponents/expopage/components/maincarousel/index.tsx
+++ frontend/src/pagecomponents/expopage/components/maincarousel/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d768d71838ecaa2ea542ebb7077522e5ea85eca730595f94c88d3715be3f5c2
-size 2813
+oid sha256:33297c93db1205a4cd3206ddc645392a6513416d906db7f6191ed1deb685154a
+size 2687
