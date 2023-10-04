2a159c24b6183f41424536650de2f20117ea546f
0214kbs <0214kbs@naver.com>
2023-10-05 00:14:14 +0900

    🎨 캐러셀 컴포넌트 분리
    Former-commit-id: 1325669e8381afd08e5bb57851c971e1a48b87f1

---
+++ frontend/src/pagecomponents/expopage/components/carousel/Carousel.styled.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7651d88ee5d14e0257664d251815d89316a75297ab65778dcd400e485bfb73c
+size 3091
---
+++ frontend/src/pagecomponents/expopage/components/carousel/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c1e14f0e60b7df4f192ac96315118c34a302f70cef34d14ae7945faf7002c16
+size 2657
