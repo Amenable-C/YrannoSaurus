c57003246d7df509130bdcec356a2d099c4492be
alsuddlek1 <minyoung99311@gmail.com>
2023-10-05 02:17:27 +0900

    :lipstick: 랜딩 애니메이션
    랜딩 애니메이션
    
    
    Former-commit-id: fe70d99633577c9c86f8a4ef19edeee3759932f2

--- frontend/src/pagecomponents/mainlanding/index.tsx
+++ frontend/src/pagecomponents/mainlanding/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:941ac2d6f36bdb9e72dbfbe6e790a245ed6d813d0c46ea5bce234541a8ef21b1
-size 1093
+oid sha256:fff10cce04ee08679ebe2179128507faf2d65c724f8adb1f2b1b5fad80297dd5
+size 1040
