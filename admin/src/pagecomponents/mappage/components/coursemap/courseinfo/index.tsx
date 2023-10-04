9ac703a5d647f331b2f9c8e77f625d291fac75a9
0214kbs <0214kbs@naver.com>
2023-10-04 14:04:01 +0900

    🎨 프로젝트 디렉토리를 보다 명확하게 재구성 - map 종류별로 폴더, 관련 컴포넌트 분리
    Former-commit-id: 3ced4f53ca4f8cf140e57ae2bf6ca715143470e6

---
+++ admin/src/pagecomponents/mappage/components/coursemap/courseinfo/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bf92a5f21a9c7339303dcaa3929d3365fd7ea35e79eb6b39e5c1045fb67e9a4
+size 1223
--- admin/src/pagecomponents/mappage/components/sidebar/index.tsx
+++ admin/src/pagecomponents/mappage/components/sidebar/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82e2c8905dc4289acb760ed70946dea6b226d469d0c60912d5c316f069931770
-size 1129
+oid sha256:0acb59dbdc4df947923789044ea500b5fe7ba016d7b226c8551c78c219fce43f
+size 1156
