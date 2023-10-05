bdd70500969bdbb20e373de25197f2e7f46e302a
김예슬 <pdy051257@naver.com>
2023-10-05 09:03:12 +0900

    :lipstick: 버튼 디자인 변경
    버튼 디자인 변경
    
    
    Former-commit-id: 780469a02b93529bc55d7ea3631da5105e464d4a

--- frontend/src/components/closebutton/index.tsx
+++ frontend/src/components/closebutton/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f23c1211fc3e82bd599fb6517806b04946812a39bc46710629ed963f7c439a72
-size 669
+oid sha256:cccac2798033b3fc6b1c2dce36fa371d72cad4ce689277d94cc1a8946c0527a3
+size 513
