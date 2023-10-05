63cbfcf22e7f5e6db2412269acfebb73b937c35f
0214kbs <0214kbs@naver.com>
2023-10-05 11:26:43 +0900

    ✨ 현재시간 기준 다음 공연 default로 선택
    Former-commit-id: ad7543822fa34f23ba60d9c591916994844db870

--- frontend/src/hooks/festival/useTodayFestivalHook.tsx
+++ frontend/src/hooks/festival/useTodayFestivalHook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:837922dc718a5ce198f902397b055ef5e32c10c7c034d18eabd93f71fd89e753
-size 1164
+oid sha256:cd7dd9be5006a0d654fb7fc1c7fbd3baa320a7a8c7b50c2bce1bd2b41c002812
+size 1905
