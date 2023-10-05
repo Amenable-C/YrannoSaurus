54bad00134ca3fa3d18d77f732db71007e31705a
0214kbs <0214kbs@naver.com>
2023-10-05 11:30:32 +0900

    💄 글자 크기 수정
    Former-commit-id: 5953e76ad4f17a5529ac029a750ffd4d50ba8cf8

--- frontend/src/pagecomponents/expopage/components/tourism/index.tsx
+++ frontend/src/pagecomponents/expopage/components/tourism/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c2567f04f6dcd1cacff18b91005e6b427adc99b6ed4aa16f0e4759b1dd64c65
+oid sha256:91b60712be7ed8f909b3b72312d85d0165fae0749157eadecd24df912b35d44f
 size 2527
