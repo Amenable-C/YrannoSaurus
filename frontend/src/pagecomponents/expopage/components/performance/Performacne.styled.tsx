f7ec7c33cc70c02ba45082f004067754ab3a907d
0214kbs <0214kbs@naver.com>
2023-10-05 14:20:53 +0900

    💄 행사장 안내 애니메이션 양옆에서 오도록 추가
    Former-commit-id: 5bc08df9f17cdebb0e4bb4b07b8737117343f477

--- frontend/src/pagecomponents/expopage/components/performance/Performacne.styled.tsx
+++ frontend/src/pagecomponents/expopage/components/performance/Performacne.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0317dccbf195f4d1976dce53f5451a5d380d385dbbe2c3c5ddd27806859b9c7
-size 1104
+oid sha256:3d1819921d9eecb7f29e1ef4124f416f26fc939d028a2aaf0c89698afa248123
+size 1756
--- frontend/src/pagecomponents/expopage/components/performance/index.tsx
+++ frontend/src/pagecomponents/expopage/components/performance/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0992fc16f611726d6e401975daa7f49f01edfc366ca13b3a34926a456427b737
-size 1484
+oid sha256:e2f8f2c2d19f84e2e6b7841e1a94c041cee02f31685b4b21ed9d1830fb05388d
+size 1649
