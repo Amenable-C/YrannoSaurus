c192299e13ca713a2b214b7dcd9f818b9bd5bc19
김예슬 <pdy051257@naver.com>
2023-10-05 15:01:23 +0900

    :memo: 로딩 화면 추가
    로딩 화면 추가
    
    
    Former-commit-id: a506fb0d94b071745b4d22444ef592f229f68f20

--- frontend/src/pagecomponents/mainlanding/index.tsx
+++ frontend/src/pagecomponents/mainlanding/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fff10cce04ee08679ebe2179128507faf2d65c724f8adb1f2b1b5fad80297dd5
+oid sha256:e224abbd95c54b1ae654a1fb7e7dba8f098c7e9d0753c1d681ebbf63955c1cd2
 size 1040
