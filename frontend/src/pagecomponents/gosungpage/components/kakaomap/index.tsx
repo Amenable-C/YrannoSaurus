ec7d62f0399f305fff45e14f53d46af343f66568
김예슬 <pdy051257@naver.com>
2023-10-05 09:10:42 +0900

    :sparkles: 지도 중심 좌표 수정
    지도 중심 좌표 수정
    
    
    Former-commit-id: b4bc3e13d6d6c1d6c423c5ec02b1c00a541bfc38

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe810db3a42ec39d393f5e28a2e5c0545e2b3bff162e9c1604efacd7a0e38d7
-size 5601
+oid sha256:f7278dc447e642677af38e1f7a823ae0a48a16847b990b38a50e3b6e44b72827
+size 5477
