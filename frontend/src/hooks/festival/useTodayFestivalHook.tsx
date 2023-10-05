4c4f927d4f73b38091e91e64b214b270f1ff78ed
0214kbs <0214kbs@naver.com>
2023-10-05 11:13:09 +0900

    ✨ 오늘의 공연 시간순으로 sorting
    Former-commit-id: 6686a6c79d8381ee95889997e868eed84958ab81

--- frontend/src/hooks/festival/useTodayFestivalHook.tsx
+++ frontend/src/hooks/festival/useTodayFestivalHook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:823d2eddbc9e4bf3e93e990327aee89f26908b1588a7f360575b0457d829323b
-size 1017
+oid sha256:837922dc718a5ce198f902397b055ef5e32c10c7c034d18eabd93f71fd89e753
+size 1164
