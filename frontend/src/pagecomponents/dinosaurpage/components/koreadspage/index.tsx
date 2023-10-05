b2e77d377d9395a67e1aa8e493bc5c4569da6f6a
0214kbs <0214kbs@naver.com>
2023-10-05 17:13:35 +0900

    🐛 대소문자
    Former-commit-id: f18d1323d60bf39ff29148e4f0084cef128a93bb

--- frontend/src/pagecomponents/dinosaurpage/components/koreadspage/index.tsx
+++ frontend/src/pagecomponents/dinosaurpage/components/koreadspage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25fd48a9920a7f7775cba74819258f139a38d4f7ec2ff50733651feb0c8720b3
+oid sha256:bdb6782acfd136b6ab8f4044856d2e070d81ee81a5feb3a4ff26e1bd9f8448cd
 size 844
