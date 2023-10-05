95897346c8abf8be868340533266c6e31c573db1
신종혁 <shinjh4903@gmail.com>
2023-10-05 17:29:41 +0900

    💄 행사장안내 사이드바 컬러 수정
    Former-commit-id: 758653ad171e3a8937e8a64b6ddf4595896bd55e

--- frontend/src/pagecomponents/guide/components/sidebar/SideBar.styled.tsx
+++ frontend/src/pagecomponents/guide/components/sidebar/SideBar.styled.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f47dec4d3f05c011fc6c7c1f9893383d9a9767f3f85708ff62ea17614a6e59d
-size 1519
+oid sha256:e9496a829a83741a863ec682177ce0b4e3547c5f57a4bc39ff0b2fc040c5568b
+size 1552
