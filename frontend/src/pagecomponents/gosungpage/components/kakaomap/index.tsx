07e585359d5e0846a588b1c1bab9dc68c1b016d0
김예슬 <pdy051257@naver.com>
2023-10-05 20:56:55 +0900

    :sparkles: 마커 초기화
    다른 카테고리 클릭시 마커 초기화
    
    
    Former-commit-id: 28e34851d679fb48e759cb6684d0cb10c3bea8d1

--- frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/kakaomap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7278dc447e642677af38e1f7a823ae0a48a16847b990b38a50e3b6e44b72827
-size 5477
+oid sha256:ec90bd7ded54cf6dcef4947a8077c8311fa5697a86c75fd232601d25d20f0ba8
+size 5698
