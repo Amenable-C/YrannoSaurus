f70955d8df0d6ef70e8dc6fc73097f2ad5dcb748
김예슬 <pdy051257@naver.com>
2023-10-05 09:08:22 +0900

    :sparkles: 맛집 업데이트
    맛집 업데이트
    
    
    Former-commit-id: 5817657a1d703d508948125794ff77ee180f4caf

--- frontend/src/pagecomponents/gosungpage/components/restaurantlist/index.tsx
+++ frontend/src/pagecomponents/gosungpage/components/restaurantlist/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df94344d94aa70e10d139621ee351db98f78ec605a847282cf5b439bf1487a1f
-size 1953
+oid sha256:92345ea0bfdb676da0a91a0be93adf72ccec71035197f5f6c389d55116719b81
+size 2811
