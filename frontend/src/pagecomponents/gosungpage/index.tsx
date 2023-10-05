a7e1c1f4986349225352218e5a0d3ec61a7b1e9a
김예슬 <pdy051257@naver.com>
2023-10-05 20:56:15 +0900

    :sparkles: 버튼 클릭 여부 표시
    버튼 클릭 여부 표시
    
    
    Former-commit-id: b29510572cfa8cc1fb268270d525834b68637e58

--- frontend/src/pagecomponents/gosungpage/index.tsx
+++ frontend/src/pagecomponents/gosungpage/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baad6402953c15c8286768e6fb2953ff46d5d34afdb6da548824491475ca487f
-size 3091
+oid sha256:71ce0b64137625f983eaac7ae6b24b963fb20a90bff866d305329bc1401be18c
+size 3323
