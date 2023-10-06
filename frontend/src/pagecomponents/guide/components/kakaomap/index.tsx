6dba95b765321c35d26aaad431b6beda9b003ab0
신종혁 <shinjh4903@gmail.com>
2023-10-06 09:22:23 +0900

    ✨ 내 위치 변경
    Former-commit-id: 91cc57a9ab49f1719c9e98bae1fc1d4ff1ae722d

--- frontend/src/pagecomponents/guide/components/kakaomap/index.tsx
+++ frontend/src/pagecomponents/guide/components/kakaomap/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b08420dc4588e705c3aec075f696786a2aefebddbed92447f6ca1dc20491d7d
-size 6329
+oid sha256:4d8dc6d08e25e684f34f8242f2d29804309b54e7526b56a6c7120fc6a873bb5d
+size 6339
